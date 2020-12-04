040853a1defacf5aea146a97ffc4a41b6e74beaa
Luke Gibbons <32064971+lukegib@users.noreply.github.com>
2020-12-04 11:45:27 +0000

    included file structure (to-do)

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-import irisDataset from 'ml-dataset-iris';
-import { DecisionTreeClassifier as DTClassifier } from 'ml-cart';
-import ConfusionMatrix from 'ml-confusion-matrix';
+// 1. Handle File 
+//  A. Is there only 1 file? shuffle and split it
+//  B. Get length of a row (no. of features)
+//  C. Convert the classes into float values
+//  D. Get the distinct classes (both string and float versions)
 
-const trainingSet = irisDataset.getNumbers(); // gets the features
-const predictions = irisDataset
-  .getClasses()
-  .map((elem) => irisDataset.getDistinctClasses().indexOf(elem)); //labels
+// 2. Use CART
+//  A. Build Tree using the training set
+//  B. Print tree to console
+//  C. Get predictions for Testing set and print to a file
 
-const options = {
-  gainFunction: 'gini',
-  maxDepth: 10,
-  minNumSamples: 3,
-};
+// 3. Testing of algorithm
+//  A. Calculate the classification accuracy
+//  B. Repeat x10
+//  C. Report on average accuracy
+//  D. Repeat steps using ml5.js implementations
+//  E. Get Confusion Matrix for two implementations
+//  F. Use learning curve/ROC curve to demonstrate
 
-const classifier = new DTClassifier(options);
-classifier.train(trainingSet, predictions);
-const result = classifier.predict(trainingSet);
-
-const CM2 = ConfusionMatrix.fromLabels(predictions, result)
-
-console.log(CM2.getAccuracy());
-
-console.log(trainingSet);
-
-console.log(predictions);
-
-console.log(irisDataset.getDataset());
+// 4. Make all above easily accessible from terminal
